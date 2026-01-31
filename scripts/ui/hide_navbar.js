fcb0f6e603eb1df60631c63fa5152842dd457f08
daniel <51289681+danielh13210@users.noreply.github.com>
2026-01-31 12:27:49 +0800

    Use true fullscreen

--- scripts/ui/hide_navbar.js
+++ scripts/ui/hide_navbar.js
@@ -1,15 +1,15 @@
 let ModeManager = {
   IS_PRODUCTION_MODE: false,
+  toggleFullscreen: ()=>{
+    if(document.fullscreenElement)
+      document.exitFullscreen();
+    else
+      document.body.requestFullscreen();
+  }
 };
-function isFullscreen_() {
-  return window.innerWidth>=screen.availWidth &&
-          window.innerHeight>=screen.availHeight;
-}
+document.addEventListener('fullscreenchange', (event) => {
+  ModeManager=!!document.fullscreenElement;
+});
 
-if(isFullscreen_()){
-  ModeManager.IS_PRODUCTION_MODE = true;
-  let navbar=document.querySelector("nav.navbar");
-  navbar.classList.add("production-hidden");
-}
-
+window.CompetitionManager=ModeManager;
 export default ModeManager;
