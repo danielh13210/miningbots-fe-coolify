cebf2fe23a4e3efbedb74bd055c9a5883e023de6
danielh13210 <danielh13210@gmail.com>
2026-06-17 11:55:39 +0800

    Make the loadingbox disappear

--- scripts/ui/loadingbox.js
+++ scripts/ui/loadingbox.js
@@ -13,13 +13,14 @@ document.head.insertAdjacentHTML("beforeend", `
 `);
 let LB_OBJECT_=document.createElement("span");
 LB_OBJECT_.id="loadingbox";
+LB_OBJECT_.classList.add("flex");
 document.querySelector(".sidebar-chrome").appendChild(LB_OBJECT_);
 LoadingBox.setStatus=function(status) {
     switch (status) {
         case LoadingBox.Status.LOADING_COMPLETED:
         case LoadingBox.Status.SERVER_NO_SELECTION:
-            LB_OBJECT_.classList.add("loading-completed");
-            LB_OBJECT_.innerHTML="";
+            LB_OBJECT_.classList.add("hidden");
+            LB_OBJECT_.classList.remove("flex");
             break;
 
         case LoadingBox.Status.LOADING:
@@ -35,7 +36,7 @@ LoadingBox.setStatus=function(status) {
 
         case LoadingBox.Status.NO_GAME:
             LB_OBJECT_.innerHTML = "No active games are available on the server";
-            LB_OBJECT_.classList.remove("loading-completed");
+            LB_OBJECT_.classList.remove("hidden");
             break;
 
 
