b6af51920c8674dab9c8dfc6259fecae503fabca
danielh13210 <danielh13210@gmail.com>
2026-06-27 12:39:40 +0800

    Replicate title behaviour

--- scripts/ui/server_text.js
+++ scripts/ui/server_text.js
@@ -1,4 +1,7 @@
 function setServerName(name){
-    document.getElementById("serverMenuButton").textContent=`Instance: ${name}`;
+    let text=`Instance: ${name}`;
+    let name_el=document.getElementById("serverMenuButton");
+    name_el.textContent=text;
+    name_el.title=text;
     document.title=`Mining Bots — ${name}`;
 }
